d79aaf073ac707e32aaddd37fe2ef151980012ac
JANMAY007 <jrbhatt18p10@gmail.com>
2024-06-07 15:23:50 +0530

    Added models in admin.py

--- Corrugation/admin.py
+++ Corrugation/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tenant, TenantEmployees, PaperReels
 
-# Register your models here.
+
+class TenantEmployeesInline(admin.TabularInline):
+    model = TenantEmployees
+    extra = 1
+
+
+class TenantAdmin(admin.ModelAdmin):
+    inlines = [TenantEmployeesInline]
+
+
+admin.site.register(Tenant, TenantAdmin)
+admin.site.register(PaperReels)
