dba1ee36446ba37e04ab9f20ddf19ee901be1963
JANMAY007 <jrbhatt18p10@gmail.com>
2024-06-08 17:21:24 +0530

    Added allauth middleware and pillow package

--- MultiBox/settings.py
+++ MultiBox/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Corrugation.middleware.TenantMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'MultiBox.urls'
